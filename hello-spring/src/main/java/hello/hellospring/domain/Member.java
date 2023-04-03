ea4cc98c05ba497e7bf3e0449b99e178017c0b31
Hojun Seo <henryseo1000@gmail.com>
2023-04-04 03:32:31 +0900

    제12강

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -12,6 +12,10 @@ public class Member {
         this.id = id;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
     public String getName(){
         return name;
     }
