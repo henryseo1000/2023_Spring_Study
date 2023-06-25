dac335c1afca185d891333b13435a470c283bb03
서호준 <henryseo1000@gmail.com>
2023-06-26 00:13:19 +0900

    제 19강

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -5,9 +5,9 @@ import hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(Long Id);
-    Optional<Member> findByName(String name);
-    List<Member> findAll();
+public interface MemberRepository {// 멤버 리포지토리 인터페이스
+    Member save(Member member);// 멤버 객체를 받아 저장한다.
+    Optional<Member> findById(Long Id);// 아이디를 받아 멤버를 조회한다.
+    Optional<Member> findByName(String name);// 이름을 받아 멤버를 조회한다.
+    List<Member> findAll();// 모든 멤버를 조회한다.
 }
